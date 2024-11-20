ee5cd99a49e9c398e981a5f8122b26391f598b88
Sam <w0292233@nscc.ca>
2024-11-19 23:07:04 -0400

    bcrypt

--- models/Course.js
+++ models/Course.js
@@ -3,10 +3,15 @@ import mongoose from 'mongoose';
 const courseSchema = new mongoose.Schema({
   name: { type: String, required: true },
   location: { type: String, required: true },
-  difficulty: { type: String, required: true },
-  holes: { type: Number, required: true },
-  par: { type: Number, required: true },
-  reviews: [{ type: String }],
+  difficulty: { type: String },
+  holes: { type: Number },
+  par: { type: Number },
+  description: { type: String },
+  coordinates: {
+    latitude: { type: Number },
+    longitude: { type: Number },
+  },
 });
 
-export default mongoose.model('Course', courseSchema);
+const Course = mongoose.model('Course', courseSchema);
+export default Course;
